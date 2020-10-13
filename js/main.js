80c47c1de6b5c677508c0db998a0a0bf68835887
marcofuenteslopez <47357668+marcofuenteslopez@users.noreply.github.com>
2020-10-13 15:46:40 -0500

    Cambio en la interfaz

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
   console.log('JS!!!');
 });
+
+
+function toggleProjects(id){
+  var projects = document.getElementById(id);
+  projects.style.display = projects.style.display == 'none' ? 'flex' : 'none';
+}
